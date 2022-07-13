fa5cacfe4ecd46cc9e992f9d8042269c3ffd8e9e
ahmetfurkankavraz <55850855+ahmetfurkankavraz@users.noreply.github.com>
2022-07-13 11:59:59 +0300

    3rd Sprint

---
+++ Regular-Question-Answers/maximum-xor-for-each-query.cpp
@@ -0,0 +1,46 @@
+// Author: Ahmet Furkan Kavraz
+// Question Link: https://leetcode.com/problems/maximum-xor-for-each-query/
+
+class Solution {
+public:
+    
+    // find results for subarrays
+    int findResult( int number, int maximumBit ){
+        
+        int result = 0;
+        
+        // k value can have at most 'maximumBit' bits
+        for (int i = 0; i < maximumBit; i++){
+            
+            // if corresponding bit is 0 in number
+            if (!(number & (1 << i)))
+                // make orresponding bit with 1 in result
+                result |= (1 << i);
+        }
+        return result;
+    }
+    
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        
+        // find xor of all numbers in array
+        int xorn = 0;
+        for (int itr: nums)
+            xorn ^= itr;
+        
+        // to store results
+        vector<int> results;
+        
+        // reverse loop
+        for (int i = nums.size() - 1; i >= 0; i--){
+            
+            // call the findResult function for each value
+            results.push_back( findResult(xorn, maximumBit) );
+            
+            // removing the last element of subarray
+            xorn ^= nums[i];
+            
+        }
+        
+        return results;
+    }
+};
