110b4df55696bed93b5c1cb18c28fcf1ede7642e
fthbaskin <baskin21@itu.edu.tr>
2022-08-30 19:10:49 +0300

    Added editorials, fthbaskin, 30th of August

---
+++ Regular-Question-Answers/reverse-bits.cpp
@@ -0,0 +1,51 @@
+// Author: Fatih Baskın
+// Reviewer:
+// Question Link: https://leetcode.com/problems/reverse-bits/
+
+/**
+ * In this question, we have been asked to revert bits of a given number.
+ * Ex: 01010000 --> 00001010
+ *
+ * For these operations, we will use unsigned 32 bit numbers (uint32_t)
+ * We will use bit manimulations extensively.
+ * & : logical and
+ * | : logical or
+ * << : left shift (multiplying by two)
+ * >> : right shift (dividing by two)
+ */
+
+class Solution 
+{
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        // We will store the number here
+        uint32_t result = 0;
+        // Left mask will be 10000....000 (1 bit on the left, rest is 0, 2^31 = 2147483648)
+        uint32_t maskL = 2147483648;
+        // Right mask will be 0000....001 (1 bit on the right, rest is 0, 2^0 = 1)
+        uint32_t maskR = 1;
+        /**
+         * If result of ANDing number and mask is not zero, mark that bit from left.
+         * Ex : (8 bits)   10101100
+         *         maskR : 00000100
+         * result of AND : 00000100
+         * Result is not 0, our right mask was at 3rd bit from right (shifted two times to left)
+         * So we will shift MaskL 2 rimes to right, and OR result (uint32_t) with mask
+         *        result : 00000000
+         *         maskL : 00100000
+         *  result of OR : 00100000
+         */
+        for(int i = 0; i < 32; i++)
+        {
+            // If ANDing number and maskR shifted left by i times is not 0
+            if(n & (maskR << i))
+            {
+                // OR result with maskL shifted right i times
+                result |= (maskL >> i);
+            }
+        }
+        // Finally return result
+        return result;
+    }
+};
