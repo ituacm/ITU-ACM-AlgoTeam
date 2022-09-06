143db8c33149555bc34c29c2772d8237913c1309
Bilgenur Çelik <71257139+bilgecelikk@users.noreply.github.com>
2022-09-06 19:56:54 +0300

    Update fruitbaskets.cpp

--- Regular-Question-Answers/fruitbaskets.cpp
+++ Regular-Question-Answers/fruitbaskets.cpp
@@ -12,7 +12,7 @@
 * if we take above example
 
 * first pass through 1s
-* firstFruit: -1 //not seen yet
+* firstFruit: -1 // not seen yet
 * secondFruit: 1
 * secFruitCount: maximum 3
 *
@@ -28,7 +28,7 @@
 * secondFruit: 0
 * secFruitCount: maximum 2
 * // here firstFruit and secondFruit exchanged positions but they were still either 1 or 0
-* // thus current_pick was always increasing in this period, maximum 12
+* // thus currentPick was always increasing in this period, maximum 12
 *
 * firstFruit: 0
 * secondFruit: 2
@@ -36,8 +36,8 @@
 * secFruitCount as before starts with 1
 * secFruitCount: maximum 3
 *
-* max_pick is the greatest of all current_pick values.
-* in our case, current_pick's {12, 5} max_pick 12.
+* maxPick is the greatest of all currentPick values.
+* in our case, currentPick's {12, 5} maxPick 12.
 */
 
 class Solution {
@@ -48,19 +48,19 @@ public:
         int firstFruit = -1, secondFruit = -1;
         // secFruitCount will be the current pick if a third value comes along
         int secFruitCount = 0;
-        // maximum of all current_pick values.
-        int max_pick = 0;
+        // maximum of all currentPick values
+        int maxPick = 0;
         // last secondFruit (and if there is, firstFruit) tree count
-        int current_pick = 0;
+        int currentPick = 0;
 
 
         for (int fruit : fruitTrees) {
-            // if the fruit is one of the wanted two fruits then increase current_pick
+            // if the fruit is one of the wanted two fruits then increase currentPick
             if (fruit == firstFruit || fruit == secondFruit)
-                current_pick++;
-            // if not, current_pick is secFruitCount and the newest fruit
+                currentPick++;
+            // if not, currentPick is secFruitCount and the newest fruit
             else
-                current_pick = secFruitCount + 1;
+                currentPick = secFruitCount + 1;
 
             // if this fruit was also equal to second one increase secFruitCount
             if (fruit == secondFruit)
@@ -76,14 +76,13 @@ public:
                 secondFruit = fruit;
             }
 
-            // compare current_pick to max_pick and update if necessary
-            max_pick = max(max_pick, current_pick);
+            // compare currentPick to maxPick and update if necessary
+            maxPick = max(maxPick, currentPick);
         }
 
-        return max_pick;
+        return maxPick;
     }
 };
 
 // Time comp.-> O(n)
 // Space comp.-> O(1)
-
