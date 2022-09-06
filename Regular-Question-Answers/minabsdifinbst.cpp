e160e1fb04f89ed5b654214b000bf6675a31fd13
Bilgenur Çelik <71257139+bilgecelikk@users.noreply.github.com>
2022-09-06 20:14:23 +0300

    Update minabsdifinbst.cpp

--- Regular-Question-Answers/minabsdifinbst.cpp
+++ Regular-Question-Answers/minabsdifinbst.cpp
@@ -14,7 +14,7 @@
 *          22        25    91
 *
 * It is asked to find the minimum difference between any two nodes.
-* Because of bst properties, its inorder traversal tree is sorted.
+* Because of bst properties, its inorder traversal form is sorted.
 * inorder form: 10 22 23 25 70 91
 * Thus, we can keep the previous value and find the difference between it and the current value.
 */
@@ -22,13 +22,13 @@
 class Solution {
 public:
     // as provided by constraints a value can't be negative, thus initialize the previous value with -1
-    int prev_val = -1;
+    int previous = -1;
     // minimum difference
-    int min_diff = INT_MAX;
+    int minDiff = INT_MAX;
 
 
     // this is an inorder traversal alike function
-    // to achieve min_diff, min_diff will be compared to the difference between
+    // to achieve minDiff, minDiff will be compared to the difference between
     // value at that position and the previous value
     void inorder(TreeNode* node) {
         // if we are below leaf nodes return
@@ -38,12 +38,12 @@ public:
         // recurse through left
         inorder(node->left);
 
-        // min_dif will be updated only if prev_val is already set
-        if (prev_val > -1)
-            min_diff = min(min_diff, node->val - prev_val);
+        // min_dif will be updated only if previous is already set
+        if (previous > -1)
+            minDiff = min(minDiff, node->val - previous);
 
-        // prev_val will be updated each step
-        prev_val = node->val;
+        // previous will be updated each step
+        previous = node->val;
 
         // recurse through right
         inorder(node->right);
@@ -52,10 +52,9 @@ public:
     int getMinimumDifference(TreeNode *root) {
         // send root to recursive function and return the updated minimum difference
         inorder(root);
-        return min_diff;
+        return minDiff;
     }
 };
 
 // Time comp.-> O(N)
 // Space comp.-> O(1)
-
