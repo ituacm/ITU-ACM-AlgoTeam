52232785e43033e31e894f792cd2fab7aa7ce242
hacerakinci <108008177+hacerakinci@users.noreply.github.com>
2022-09-06 21:29:56 +0300

    Update minimum_recolors_to_get_k_consecutive_black_blocks.cpp

--- minimum_recolors_to_get_k_consecutive_black_blocks.cpp
+++ minimum_recolors_to_get_k_consecutive_black_blocks.cpp
@@ -1,7 +1,38 @@
 // Author: Hacer Akıncı
 // Question Link: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
-// Time compexity: O(n) 
-// Space compexity: O(1) 
+
+/*
+ To find minimum recolors to get K consecutive black blocks, 
+ we should check every substring occurs K blocks for the number of white blocks in that substring.
+ Minimum white block number is equals minimum operation to recolor.
+
+ For the first substring, calculate the white block numbers and keep it. 
+ For the rest of that, if next value equals 'W' increase white block numbers by one,
+ if first value of previous substring equals 'W' decrease white block numbers by one,
+ because it is not include new substring.
+ 
+For example, blocks = "WBBWWBBWBW", k = 7
+
+First substring:
+         "WBBWWBBWBW"
+          ^     ^
+          |     | 
+        i = 0   i = 6
+whiteBlocks = 3;
+
+Second substring:
+         "WBBWWBBWBW"
+          ^      ^
+          |      |
+        i = 0   i = 7
+s[7] = 'W' whiteBlocks++;
+s[0] = 'W' whiteBlocks--;
+
+whiteBlocks = 3;
+
+And it so on until the end of the string, 
+minOperaion is the minimum of all whiteBlocks
+*/
 
 class Solution {
 public:
@@ -15,15 +46,17 @@ public:
             if(blocks[i] == 'W') whiteBlocks++;
         }
         
-        // We initalize minOperation with first value of whiteBlocks. Then check rest of the string for minimum value . 
+        // We initalize minOperation with first value of whiteBlocks,
         int minOperation = whiteBlocks;
         
-        while(i < blocks.size()){
+        // Check rest of the string for minimum value . 
+        for(i = k; i < blocks.size(); i++){
             
-            // If next block is white, increase by one
+            // If ith block, last eleman of new substring, is white, increase by one
             if(blocks[i] == 'W') whiteBlocks++;
             
-            // We should check (i-k). block because it is no more in substring, if it is white we should decrease by one
+            // We should check (i-k). block because it is no more in new substring,
+            // if it is white we should decrease by one
             if(blocks[i-k] == 'W') whiteBlocks--;
             
             // If the white block number of new substring is smaller, assign it to minOperation   
@@ -31,10 +64,12 @@ public:
             
             // Since it cannot be less than zero, return 0
             if(minOperation == 0) return 0;
-            
-            i++;
+
         }
         
         return minOperation;
     }
 };
+
+// Time complexity: O(n) 
+// Space complexity: O(1) 
