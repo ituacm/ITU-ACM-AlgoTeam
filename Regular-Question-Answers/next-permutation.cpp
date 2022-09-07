26388a3cbc744655ca14ce9ac317e21affa2dfab
Toygun Görmüş <93976263+grmstoygun@users.noreply.github.com>
2022-09-07 13:07:27 +0300

    Update
    updated alignment

--- Regular-Question-Answers/next-permutation.cpp
+++ Regular-Question-Answers/next-permutation.cpp
@@ -5,30 +5,30 @@ The two pointers will point to the first not sorted element and the first greate
 Thus, by swapping these numbers and sorting the rest of the array from the first not sorted element should yield the next permutation.  */
 class Solution {
 public:
-void nextPermutation (vector<int>& nums) {
+    void nextPermutation (vector<int>& nums) {
 
-    // If the array has only one elements, the next permutation is itself. 
-    if (nums.size() == 1) return;
+        // If the array has only one elements, the next permutation is itself. 
+        if (nums.size() == 1) return;
     
-    // We use l pointer to detect first not sorted element.
-    int l = nums.size()-2;
-    while (nums[l] >= nums[l+1]){
-    // Checking if the array is descending. If so, the reverse of the array is the next permutation.
-    // (i.e. Input: nums = [3,2,1] Output: [1,2,3])
-        if (l == 0) {
-            reverse (nums.begin(), nums.end());
-            return;
+        // We use l pointer to detect first not sorted element.
+        int l = nums.size()-2;
+        while (nums[l] >= nums[l+1]){
+        // Checking if the array is descending. If so, the reverse of the array is the next permutation.
+        // (i.e. Input: nums = [3,2,1] Output: [1,2,3])
+            if (l == 0) {
+                reverse (nums.begin(), nums.end());
+                return;
+            }
+            l--;   
         }
-        l--;   
-    }
     
-    // We use r pointer to detect the first greater element than left
-    int r = nums.size()-1;
-    while (nums[l] >= nums[r]) r--;
+        // We use r pointer to detect the first greater element than left
+        int r = nums.size()-1;
+        while (nums[l] >= nums[r]) r--;
     
-    // Swapping the numbers at indexes l and r to find the next permutation in lexicographical order
-    swap (nums[l], nums[r]);
-    // Sorting the rest of the array yields the answer
-    sort (nums.begin()+l+1, nums.end());
+        // Swapping the numbers at indexes l and r to find the next permutation in lexicographical order
+        swap (nums[l], nums[r]);
+        // Sorting the rest of the array yields the answer
+        sort (nums.begin()+l+1, nums.end());
     }
 };
