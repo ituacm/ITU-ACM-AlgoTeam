3a6a96e8d33b81939acab1bca44b178d9d6bfc22
Toygun Görmüş <93976263+grmstoygun@users.noreply.github.com>
2022-09-07 12:39:28 +0300

    Update No.2 with Bilge's review

--- Regular-Question-Answers/next-permutation.cpp
+++ Regular-Question-Answers/next-permutation.cpp
@@ -1,38 +1,34 @@
-//Author: Toygun Görmüş
-//Problem Link : https://leetcode.com/problems/next-permutation/
+// Author: Toygun Görmüş
+// Problem Link : https://leetcode.com/problems/next-permutation/
 /* Using two pointers will be efficient in this problem.
 The two pointers will point to the first not sorted element and the first greater element after it. 
 Thus, by swapping these numbers and sorting the rest of the array from the first not sorted element should yield the next permutation.  */
 class Solution {
 public:
-    // Declaring a function to check if the given array is in descending order.
-    bool descending(vector<int>& nums){
-        // If every number is smaller than the previous number in every iteration, the array is descendant. 
-    for(int i =nums.size()-2;i>=0;i--){
-        if(nums[i]<nums[i+1]){
-             return 0;
-        }
-    }
-    return 1;
-    }
-void nextPermutation(vector<int>& nums){
-    // Checking if the array is descending. If so, the reverse of the array is the next permutation
-    // (i.e. Input: nums = [3,2,1] Output: [1,2,3]).
-    if(descending(nums)){
-        reverse(nums.begin(),nums.end());
-        return;
-    }
-    // We use l pointer to detect first not sorted element
+void nextPermutation (vector<int>& nums) {
+
+    // If the array has only one elements, the next permutation is itself. 
+    if(nums.size()==1) return;
+    
+    // We use l pointer to detect first not sorted element.
     int l = nums.size()-2;
-    while(nums[l]>=nums[l+1]) l--;
+    while (nums[l] >= nums[l+1]){
+    // Checking if the array is descending. If so, the reverse of the array is the next permutation.
+    // (i.e. Input: nums = [3,2,1] Output: [1,2,3])
+        if (l == 0) {
+            reverse (nums.begin(), nums.end());
+            return;
+        }
+        l--;   
+    }
     
     // We use r pointer to detect the first greater element than left
     int r = nums.size()-1;
-    while(nums[l]>=nums[r])r--;
+    while (nums[l] >= nums[r]) r--;
     
     // Swapping the numbers at indexes l and r to find the next permutation in lexicographical order
-    swap(nums[l], nums[r]);
+    swap (nums[l], nums[r]);
     // Sorting the rest of the array yields the answer
-    sort(nums.begin()+l+1, nums.end());
+    sort (nums.begin()+l+1, nums.end());
     }
 };
