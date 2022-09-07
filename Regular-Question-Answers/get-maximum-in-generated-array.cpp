215958d126de7e5b682ef5bd3c7a8ba1c8154eab
omer-faruk-erdem <111380603+omer-faruk-erdem@users.noreply.github.com>
2022-09-07 15:28:23 +0300

    Update get-maximum-in-generated-array.cpp

--- Regular-Question-Answers/get-maximum-in-generated-array.cpp
+++ Regular-Question-Answers/get-maximum-in-generated-array.cpp
@@ -1,13 +1,16 @@
 // Author: Ömer Faruk ERDEM
 // Question Link: https://leetcode.com/problems/get-maximum-in-generated-array/
 
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
 class Solution {
 public:
-   /* In the question, we are given an integer n and we are asked to create an array of size n+1 as written in the question.
-    * And we will return the maximum element in the array.
+    /* In the question, we are given an integer n and we are asked to create an array of size n+1 as written in the question.
+       And we will return the maximum element in the array. 
     */
     int getMaximumGenerated(int n) {
-        //if the input is zero then we will return 0 since 0+1 sized array's maximum element is 0.
+        // If the input is zero then we will return 0 since 0+1 sized array's maximum element is 0.
         if (n==0) return 0;
 
         // Creating n+1 sized vector and initialize all of them with 0.
@@ -20,19 +23,19 @@ public:
         nums[0] = 0;
         nums[1] = 1;
 
-        //Now it is time to create our array.
-        //First create a loop from 1 to n/2.
+        // Now it is time to create our array.
+        // First create a loop from 1 to n/2.
         for (int i = 1 ; i <= n/2 ; i++){
             
             // And check the conditions that we are given in the question.
-            if ( 2<= 2*i and 2*i<=n ){
+            if ( 2<= 2*i && 2*i<=n ){
                 nums[2*i] = nums[i];
 
                 // And check if new created element is greater then our current max.
                 // Assign maximum element to maximum of current maximum element's value and new created element.
                 maximumElement = max(nums[2*i],maximumElement);
             }
-            if ( 2<= 2*i+1 and 2*i+1<=n  ){
+            if ( 2<= 2*i+1 && 2*i+1<=n  ){
                 nums[2*i+1] = nums[i]+nums[i+1];
 
                 // And again, assign maximum element to maximum of current maximum element's value and new created element.
