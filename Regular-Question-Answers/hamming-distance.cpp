2e22314b9a72cb3b3cb882c6346e7480a3ecd6ee
fthbaskin <cs.fthbaskin@gmail.com>
2022-10-01 16:18:09 +0300

    __builtin_popcount explained

--- Regular-Question-Answers/hamming-distance.cpp
+++ Regular-Question-Answers/hamming-distance.cpp
@@ -6,6 +6,11 @@
  * If you XOR two numbers, different bits will be 1 and same bits will be 0.
  * 0^0 = 0, 1^1 = 0, 0^1 = 1
  * With XOR and builtin popcount (returns number of 1s), we can find hamming distance.
+ *
+ * __builtin_popcount example:
+ * A therotical variable x represented with 8 bits: 0100 1001
+ * If we call __builtin_popcount function, it will return 3 because there are 3 bits
+ * set as 1.
  */
 
 class Solution 
