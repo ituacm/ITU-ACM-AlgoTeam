7c9a888769e9bcfe6c2926a2661dee4886057274
Bilgenur Çelik <71257139+bilgecelikk@users.noreply.github.com>
2022-10-04 15:30:51 +0300

    Update is-subsequence.cpp

--- Regular-Question-Answers/is-subsequence.cpp
+++ Regular-Question-Answers/is-subsequence.cpp
@@ -1,28 +1,32 @@
 // Author: Bilgenur Çelik
+// Reviewer: Novruz Amirov
 // Question Link: https://leetcode.com/problems/is-subsequence/
 
 class Solution {
 public:
+    /* We want to know if the t string contains the s string's letters as a subsequence. 
+    *  With two pointers one pointing at the beginning of the t, another one to the s.
+    *  Move along the t string's letters. If the letter matches the s pointer's letter, point to the next value.
+    *  If the s pointer's value is equal to the length of the s, it is a subsequence.
+    *  If yes return true, else false.
+    */
     bool isSubsequence(string s, string t) {
-        // Two pointers will help us.
-
         // base case:
-        if(s == ""){
+        if (s == "")
             return true;
-        }
 
         // sPointer will increase if letters match.
         int sPointer = 0;
 
-        // i is the t's pointer. It will increase whether or not they match.
-        for (int i = 0; i < t.length(); i++){
+        // tPointer is the t's pointer. It will increase whether or not the characters match.
+        for (int tPointer = 0; tPointer < t.length(); tPointer++){
             // into a matching state:
-            if (t[i] == s[sPointer]){
+            if (t[tPointer] == s[sPointer]){
                 // increase sPointer
                 sPointer++;
 
                 // if the s array comes to an end, then it is a subsequence of t
-                if(sPointer == s.length())
+                if (sPointer == s.length())
                     return true;
             }
         }
@@ -31,3 +35,6 @@ public:
         return false;
     }
 };
+
+// Time comp. -> O(n) Space comp. -> O(1)
+
