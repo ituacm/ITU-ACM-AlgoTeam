9561952c23ac16d433d27b978868c9f026ea2d34
Bilgenur Çelik <71257139+bilgecelikk@users.noreply.github.com>
2022-10-05 18:06:31 +0300

    Update is-subsequence.cpp

--- Regular-Question-Answers/is-subsequence.cpp
+++ Regular-Question-Answers/is-subsequence.cpp
@@ -1,12 +1,14 @@
 // Author: Bilgenur Çelik
 // Reviewer: Novruz Amirov
 // Question Link: https://leetcode.com/problems/is-subsequence/
+// Time comp. -> O(n) (n = length of the string t)
+// Space comp. -> O(1)
 
 class Solution {
 public:
     /* We want to know if the t string contains the s string's letters as a subsequence. 
     *  With two pointers one pointing at the beginning of the t, another one to the s.
-    *  Move along the t string's letters. If the letter matches the s pointer's letter, point to the next value.
+    *  Move along the t string's letters. If the letter matches the s pointer's letter, point to the next value of s.
     *  If the s pointer's value is equal to the length of the s, it is a subsequence.
     *  If yes return true, else false.
     */
@@ -35,6 +37,3 @@ public:
         return false;
     }
 };
-
-// Time comp. -> O(n) (n = length of the string t)
-// Space comp. -> O(1)
