50a8806846dd6fad0821f3258b875ce6123a3a4c
feyza sarı <112571235+feyzzs@users.noreply.github.com>
2022-10-10 01:28:52 +0300

    Update longest-substring-without-repeating-characters.cpp
    fix

--- longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,34 @@
 // Author: Feyza Sarı
-// Question Link:
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Reviewers: Bilgenur Çelik, Denis Davidoğlu
+// Question Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 // Sliding Window Algorithm:
 class Solution {
 public:
     int lengthOfLongestSubstring(string strng) {
-        // creating a vector for checking if the character is in alrady in substring
-        // -1 for character that we did not see before
-        // we store place of the character for every character that we saw
+        // create a vector for checking if the character is already in the substring
+        // -1 for the character that we did not see before
+        // we store the place of the character for every character that we saw
         vector<int> mpp(256, -1);
 
-        // left is beggining of current substring
-        // right is end of current substring
-        // thus lenght of current substring is right-left+1
-        int left = 0, right = 0;
-        int n = strng.size();
-        int lenghtOfSubstring = 0; // maximum lenght of the substring without repeating
-        // we search while we are in the given string
+        // left is the beginning of the current substring
+        // right is the end of the current substring
+        // thus length of current substring is right-left+1
+        // lengthOfSubstring is the maximum length of the substring without repeating
+        int left = 0, right = 0; n = strng.size(); lengthOfSubstring = 0; 
+        // we should search while we are in the given string
         while (right < n) {
             if (mpp[strng[right]] != -1) // if the character is repeating
                 left = max(
                 mpp[strng[right]] + 1,
                 left); // left will be the next character of the repating character
 
-            mpp[strng[right]] = right; // storing place of the characters
+            mpp[strng[right]] = right; // store the place of the characters
 
-            lenghtOfSubstring = max(lenghtOfSubstring,
-                right - left + 1); // if current substring is longer than the
-                                 // previous ones update the maximum lenght
-            right++; // add a new character to substing for checkin if it is repating
-               // or not
+            lengthOfSubstring = max(lengthOfSubstring,
+                right - left + 1); // if the current substring is longer than the previous ones update the maximum length
+            right++; // add a new character to the substring to check if it is repeating or not
         }
-    return lenghtOfSubstring; // return the maximum lenght
+    return lengthOfSubstring; // return the maximum length
   }
 };
