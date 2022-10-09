3342b63bf7e9d63d217a41c4d5f6c5ba12d67e52
feyza sarı <112571235+feyzzs@users.noreply.github.com>
2022-10-10 01:45:45 +0300

    Update remove-element.cpp
    fix

--- remove-element.cpp
+++ remove-element.cpp
@@ -1,28 +1,31 @@
 // Author:Feyza Sarı
+// Reviewers: Bilgenur Çelik, Denis Davidoglu
 // Question Link: https://leetcode.com/problems/remove-element/
 // O(n)
 
 /* First we have to check our element is equal to val.
- If it is not nothiing change. But if they are equal we have to remove the
- element. When we remove an element our array decrease one. So if we swap the
- element - that equals to val - and the last element of array and decrease the
- size of array we get the wanted array. */
+ If it is not nothing changes. But if they are equal we have to remove the
+ element. When we remove an element our array decreases one. So if we swap the
+ element - that equals val - and the last element of the array and decrease
+ the size of the array we get the wanted array. */
 class Solution {
 public:
     int removeElement(vector<int> &nums, int val) {
-        int i = 0;                  // current place
-        int size = nums.size() - 1; // array size
+        //i is current place, size is array size
+        int i = 0;                 
+        int size = nums.size() - 1; 
 
-        // search the array while we ar in the array
+        // search the array while we are in the array
         while (i <= size) {
             // check if they are equal
-            // swap the elements, decrease the size of array(size--)
+            // swap the elements, decrease the size of the array(size--)
             if (nums[i] == val)
                 swap(nums[i], nums[size--]);
+            /* we didn't increase i because when we swap the elements, i'th element is changed with a new value (the last element of the array). So we should check i'th elemet again.*/
             else
             i++; // skip to the next element
         }
 
-        return size + 1; // return the size of array that wanted
+        return size + 1; // return the size of the array that wanted
     }
 };
