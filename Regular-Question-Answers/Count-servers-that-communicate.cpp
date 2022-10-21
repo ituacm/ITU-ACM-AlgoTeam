ca2ec185267bc43ecd9016459f69d1c038729ce4
Emre Çelik <73967717+ituitis20-celikem19@users.noreply.github.com>
2022-10-21 18:38:29 +0300

    Update Count-servers-that-communicate.cpp

--- Regular-Question-Answers/Count-servers-that-communicate.cpp
+++ Regular-Question-Answers/Count-servers-that-communicate.cpp
@@ -1,7 +1,9 @@
 // Author: Emre Çelik
 // Question Link: https://leetcode.com/problems/count-servers-that-communicate/
+// Reviewer: Ceren Yaşar
 
-// Complexity: O(n*m)
+// Time Complexity: O(n*m)
+// Memory Complexity: O(n*m)
 
 class Solution {
 public:
@@ -10,7 +12,7 @@ public:
         // Declaring n and m as number of columns and rows.
         int n = grid.size();
         int m = grid[0].size();
-        // Declaring row and col vectors for how many servers in that row or column. 
+        // Declaring row and col vectors with the number of servers in that row or column.
         vector<int> row(n), col(m);
         
         // Search our grid to calculate number of server for row and col vectors.
@@ -24,11 +26,11 @@ public:
             }
         }
         
-        // Number of server that are communicate with at least one of other servers.
+        // Number of servers that communicate with at least one of the other servers.
         int total = 0;
         
-        /* Checking our grid if ith row or jth column has at least 2 servers 
-        to communicate with each other. */
+        /* Checking if ith row and jth column of our our grid have at least 2 servers
+        to communicate with each other.*/
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(grid[i][j] == 1 && (row[i] >= 2 || col[j] >= 2)) {
