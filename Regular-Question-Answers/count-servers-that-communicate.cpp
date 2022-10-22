450e0c1191632100c068aefa4d73bab7785e1c97
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-10-22 14:50:55 +0300

    Update and rename Count-servers-that-communicate.cpp to count-servers-that-communicate.cpp

--- Regular-Question-Answers/count-servers-that-communicate.cpp
+++ Regular-Question-Answers/count-servers-that-communicate.cpp
@@ -1,6 +1,6 @@
 // Author: Emre Çelik
 // Question Link: https://leetcode.com/problems/count-servers-that-communicate/
-// Reviewer: Ceren Yaşar
+// Reviewer: Ceren Yaşar, Denis Davidoglu
 
 // Time Complexity: O(n*m)
 // Memory Complexity: O(n*m)
@@ -8,7 +8,6 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        
         // Declaring n and m as number of columns and rows.
         int n = grid.size();
         int m = grid[0].size();
@@ -16,10 +15,10 @@ public:
         vector<int> row(n), col(m);
         
         // Search our grid to calculate number of server for row and col vectors.
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 // Decreasing row[i] and col[j] if grid[i][j] has server.
-                if(grid[i][j] == 1) {
+                if (grid[i][j] == 1) {
                     row[i]++;
                     col[j]++;
                 }
@@ -31,9 +30,9 @@ public:
         
         /* Checking if ith row and jth column of our our grid have at least 2 servers
         to communicate with each other.*/
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(grid[i][j] == 1 && (row[i] >= 2 || col[j] >= 2)) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && (row[i] >= 2 || col[j] >= 2)) {
                     // If states are ensured, decrease total by 1.
                     total++;
                 }
