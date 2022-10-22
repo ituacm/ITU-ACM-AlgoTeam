40fecdf652daae8f624e8c2e577897bb5f569e99
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-10-22 14:49:22 +0300

    Update and rename Base-7.cpp to base-7.cpp

--- Regular-Question-Answers/base-7.cpp
+++ Regular-Question-Answers/base-7.cpp
@@ -1,6 +1,6 @@
 // Author: Emre Çelik
 // Question Link: https://leetcode.com/problems/base-7/
-// Reviewer: Ceren Yaşar
+// Reviewer: Ceren Yaşar, Denis Davidoglu
 
 // Time Complexity: O(logn)
 // Memory Complexity: O(1)
@@ -12,23 +12,24 @@ public:
         // distance to 0 and then convert it to its actual value, 
         // which can be positive or negative.
         int n = abs(num);
+        
         // Declaring ans string for our answer.
         string ans = "";
         
         // If number is 0 directly return "0" as a string.
-        if(num == 0) {
+        if (num == 0) {
             ans = "0";
             return ans;
         }
         
         // Creating our ans string with mod and divide n by 7.
-        while(n > 0) {
+        while (n > 0) {
             ans = to_string(n%7) + ans;
             n = n/7;
         }
         
         // Check number and if number is negative add "-" to our ans string.
-        if(num < 0) {
+        if (num < 0) {
             ans = "-" + ans;
         }
             
