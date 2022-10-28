4dc2ae5146be34acdd838ea0f8a263203944f751
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-10-28 16:53:59 +0300

    Brute review next-permutation.cpp

--- Regular-Question-Answers/next-permutation.cpp
+++ Regular-Question-Answers/next-permutation.cpp
@@ -1,22 +1,24 @@
 // Author: Toygun Görmüş
-// Problem Link : https://leetcode.com/problems/next-permutation/
+// Question Link : https://leetcode.com/problems/next-permutation/
+// Reviewers: Bilgenur Çelik, Denis Davidoglu
+
 /* Using two pointers will be efficient in this problem.
 The two pointers will point to the first not sorted element and the first greater element after it. 
 Thus, by swapping these numbers and sorting the rest of the array from the first not sorted element should yield the next permutation.  */
+
 class Solution {
 public:
     void nextPermutation (vector<int>& nums) {
-
-        // If the array has only one elements, the next permutation is itself. 
+        // If the array has only one element, the next permutation is itself. 
         if (nums.size() == 1) return;
     
         // We use l pointer to detect first not sorted element.
         int l = nums.size()-2;
-        while (nums[l] >= nums[l+1]){
-        // Checking if the array is descending. If so, the reverse of the array is the next permutation.
-        // (i.e. Input: nums = [3,2,1] Output: [1,2,3])
+        while (nums[l] >= nums[l+1]) {
+            // Checking if the array is descending. If so, the reverse of the array is the next permutation.
+            // (i.e. Input: nums = [3,2,1] Output: [1,2,3])
             if (l == 0) {
-                reverse (nums.begin(), nums.end());
+                reverse(nums.begin(), nums.end());
                 return;
             }
             l--;   
@@ -27,8 +29,8 @@ public:
         while (nums[l] >= nums[r]) r--;
     
         // Swapping the numbers at indexes l and r to find the next permutation in lexicographical order
-        swap (nums[l], nums[r]);
+        swap(nums[l], nums[r]);
         // Sorting the rest of the array yields the answer
-        sort (nums.begin()+l+1, nums.end());
+        sort(nums.begin()+l+1, nums.end());
     }
 };
