6ac5aa4c22cd401cccc368c840795e5a4be5736d
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-10-30 23:52:13 +0300

    Brute review is-subsequence.cpp

--- Regular-Question-Answers/is-subsequence.cpp
+++ Regular-Question-Answers/is-subsequence.cpp
@@ -1,6 +1,7 @@
 // Author: Bilgenur Çelik
-// Reviewer: Novruz Amirov
 // Question Link: https://leetcode.com/problems/is-subsequence/
+// Reviewers: Novruz Amirov, Ömer Faruk Erdem, Denis Davidoglu
+
 // Time comp. -> O(n) (n = length of the string t)
 // Space comp. -> O(1)
 
@@ -14,26 +15,23 @@ public:
     */
     bool isSubsequence(string s, string t) {
         // base case:
-        if (s == "")
-            return true;
+        if (s == "") return true;
 
         // sPointer will increase if letters match.
         int sPointer = 0;
 
         // tPointer is the t's pointer. It will increase whether or not the characters match.
-        for (int tPointer = 0; tPointer < t.length(); tPointer++){
+        for (int tPointer = 0; tPointer < t.length(); tPointer++) {
             // into a matching state:
-            if (t[tPointer] == s[sPointer]){
-                // increase sPointer
+            if (t[tPointer] == s[sPointer]) {
                 sPointer++;
 
                 // if the s array comes to an end, then it is a subsequence of t
-                if (sPointer == s.length())
-                    return true;
+                if (sPointer == s.length()) return true;
             }
         }
 
-        // in any other occasion:
+        // in any other case:
         return false;
     }
 };
