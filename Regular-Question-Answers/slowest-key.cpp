7d6b181e039eb847b3f86f59acca95ab16f3689d
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-11-14 00:41:14 +0300

    Update slowest-key.cpp

--- Regular-Question-Answers/slowest-key.cpp
+++ Regular-Question-Answers/slowest-key.cpp
@@ -1,8 +1,11 @@
 // Author: Toygun Görmüş
 // Question Link: https://leetcode.com/problems/slowest-key/
+// Reviewers: Hacer Akıncı, Denis Davidoglu
+
 /* We need to traverse the releaseTimes array and calculate the pressing time for every key. 
 Then we need to evaluate the largest of these times and get the larger one lexicographically if there is an equality.
 */
+
 class Solution {
 public:
     char slowestKey (vector<int>& releaseTimes, string keysPressed) {
@@ -13,14 +16,14 @@ public:
         int max_time = releaseTimes[0];
         char max_key = keysPressed[0];
         
-        for (int i = 1; i < releaseTimes.size(); i++){
+        for (int i = 1; i < releaseTimes.size(); i++) {
             time = releaseTimes[i] - releaseTimes[i - 1];
             // If the time is larger then the maximum value, we update the maximum value and the key it represents.
             if (time > max_time){
                 max_time = time;
                 max_key = keysPressed[i];
             }
-            // If the time is equal to the maximum value we get the lexicographically greater one using max fnuction.
+            // If the time is equal to the maximum value we get the lexicographically greater one using max function.
             else if (time == max_time){
                 max_key = max (max_key, keysPressed[i]);
             }
