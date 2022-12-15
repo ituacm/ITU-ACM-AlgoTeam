6748e9e5b8d0981a9ae8222871f4c13df546db90
Bilgenur Çelik <71257139+bilgecelikk@users.noreply.github.com>
2022-12-15 22:53:12 +0300

    52. sprint

---
+++ Regular-Question-Answers/heaters.cpp
@@ -0,0 +1,43 @@
+// Author: Bilgenur Çelik
+// Question Link: https://leetcode.com/problems/heaters/
+// Time Comp.: O(nlog(m)) n = houses.size() m = heaters.size()
+
+class Solution {
+public:
+    /* With this helper find the closest heater's distance using binary seach.*/
+    int helper(int house, vector<int>& heaters, int begin, int end) {
+
+        int dist = INT_MAX;
+        int mid;
+
+        while(begin < end) {
+            mid = (begin + end) / 2;
+            // check each mid-point's distance.
+            dist = min(abs(heaters[mid] - house), dist);
+
+            // if closers are on left:
+            if(heaters[mid] > house)
+                end = mid;
+            // if closers are on right:
+            else
+                begin = mid+1;
+        }
+
+        return dist;
+    }
+
+
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        // sort heaters to apply binary search on this vector
+        sort(heaters.begin(), heaters.end());
+        // minimum radius will be the most comprehensive radius
+        int minRadius = INT_MIN;
+
+        // find the closest heater's distance for each house
+        for(int i : houses){
+            minRadius = max(minRadius, helper(i, heaters, 0, heaters.size()));
+        }
+
+        return minRadius;
+    }
+};
---
+++ Regular-Question-Answers/number-complement.cpp
@@ -0,0 +1,25 @@
+// Author: Bilgenur Çelik
+// Question Link: https://leetcode.com/problems/number-complement/
+
+class Solution {
+public:
+    int findComplement(int num) {
+        /* Complement of an integer is the inverted binary status of the integer.
+        Mathematically we can get (2^m - 1) at maximum, m = binary digit size of the number.
+        Thus substract num from maximum to get the result.
+        */
+        // binary lenght
+        int binLenght = 0;
+
+        // hold a temp value for division
+        int temp = num;
+
+        while(temp > 0) {
+            binLenght++;
+            // division by two
+            temp = temp >> 1;
+        }
+
+        return (pow(2, binLenght) - 1) - num;
+    }
+};
