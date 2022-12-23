d582495ff7c1703146510b09dcb83bd12fe9ff6c
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-12-23 18:59:38 +0300

    Update number-complement.cpp

--- Regular-Question-Answers/number-complement.cpp
+++ Regular-Question-Answers/number-complement.cpp
@@ -1,10 +1,11 @@
 // Author: Bilgenur Çelik
 // Question Link: https://leetcode.com/problems/number-complement/
+// Reviewer: Denis Davidoglu
 
 class Solution {
 public:
     int findComplement(int num) {
-        /* Complement of an integer is the inverted binary status of the integer.
+        /* Complement of an integer is the inverted binary state of the integer.
         Mathematically we can get (2^m - 1) at maximum, m = binary digit size of the number.
         Thus substract num from maximum to get the result.
         */
@@ -14,7 +15,7 @@ public:
         // hold a temp value for division
         int temp = num;
 
-        while(temp > 0) {
+        while (temp > 0) {
             binLenght++;
             // division by two
             temp = temp >> 1;
