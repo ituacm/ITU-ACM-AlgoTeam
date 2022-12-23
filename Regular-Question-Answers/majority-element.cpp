576c842395501440cc4b4ce284c25b538bf62aee
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2022-12-23 19:15:36 +0300

    Update majority-element.cpp

--- Regular-Question-Answers/majority-element.cpp
+++ Regular-Question-Answers/majority-element.cpp
@@ -1,26 +1,19 @@
 // Author: Emre ÇELİK
 // Question Link: https://leetcode.com/problems/majority-element/
-// Reviewers:
+// Reviewer: Denis Davidoglu
 
-// Time Complexity : O(n)
+// Time Complexity : O(nlogn)
 // Space Complexity : O(1)
-
-//   If nums.size()%2==0
-//      | | | |
-//          ^ this is important for us
-//   If nums.size()%2==1
-//      | | | | |
-//          ^ this is important for us
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //Firstly we are sorting our vector to find
-        //the mid number if nums.size() is odd or 
-        //mid+1 number if nums.size() is even.
+        // Array can be first sorted to make alike elements adjacent 
         sort(nums.begin(), nums.end());
-        //Return our data with (nums.size()/2)th index,
-        //while we are working with 0 index.
+        
+        // If the majority elements appears more than n/2 times,
+        // no matter where they begin, there will always be one
+        // in the middle of sorted array.
         return nums[(nums.size()/2)];
     }
 };
