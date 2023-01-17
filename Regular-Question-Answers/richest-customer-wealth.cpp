1b15e2b8ba285aef03d2c7dba57ca0a6b50ef195
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-01-18 01:04:20 +0300

    Update richest-customer-wealth.cpp

--- Regular-Question-Answers/richest-customer-wealth.cpp
+++ Regular-Question-Answers/richest-customer-wealth.cpp
@@ -1,22 +1,22 @@
 // Author: Rojen Arda Şeşen
 // Question Link: https://leetcode.com/problems/richest-customer-wealth/
+// Reviewer: Denis Davidoglu
 
 // Time Compexity: O(n x m) where accounts is an n x m matrix.
-
 
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxWealth = INT_MIN;
-        for (int i = 0; i < accounts.size(); i++) { // Looping through each customer
+        // Looping through each customer
+        for (int i = 0; i < accounts.size(); i++) { 
             int sum = 0; // ith customer's total wealth.
             // Summing each customer's wealth from all accounts.
             for (int j = 0; j < accounts[i].size(); j++) { 
                 sum += accounts[i][j];
             }
             // If the new sum is greater than the previous maximum value, update the maximum value.
-            if (sum > maxWealth) 
-                maxWealth = sum;
+            if (sum > maxWealth) maxWealth = sum;
         }
         return maxWealth;
     }
