7db2051522439f116ecfe9b3ef6a265e9ab3c2bb
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-01-18 00:13:34 +0300

    Update middle-of-the-linked-list.cpp

--- Regular-Question-Answers/middle-of-the-linked-list.cpp
+++ Regular-Question-Answers/middle-of-the-linked-list.cpp
@@ -1,5 +1,6 @@
-// Question: https://leetcode.com/problems/middle-of-the-linked-list/description/
 // Author: Murat Biberoğlu
+// Question: https://leetcode.com/problems/middle-of-the-linked-list/
+// Reviewer: Denis Davidoglu
 
 /**
  * Definition for singly-linked list.
@@ -11,29 +12,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ 
 class Solution {
 public:
-    // the idea is holding two pointer to nodes which are slow and fast
-    // slow moves 1 node each step and fast moves 2 node at each step until fast is not null
-    // we will move slow first and then move fast because 
+    // the idea is holding two pointers to nodes, of which one is slow and one is fast
+    // the slow one moves 1 node each step and the fast one moves 2 nodes at each step until fast is not null
+    // we will move the slow one first and then move the fast one, because 
     // we want return the second middle node if there are two middle nodes 
     ListNode* middleNode(ListNode* head) {
         // if list is empty return null
-        if(!head) return nullptr;
-
+        if (!head) return nullptr;
         // if list consists of one node return head
-        else if(!head->next) return head;
+        else if (!head->next) return head;
 
         // initialize pointers
         ListNode* fast = head->next;
         ListNode* slow = head;
 
-        while(fast) {
+        while (fast) {
             slow = slow->next;
             fast = fast->next;
 
             // prevent segmentation fault
-            if(fast) fast = fast->next;
+            if (fast) fast = fast->next;
         }
         return slow;
     }
