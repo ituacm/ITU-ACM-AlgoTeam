9c5f8f33b1a312563793c3da7c886c1b3653f0aa
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-01-25 21:01:40 +0300

    Update repeated-string-match.cpp

--- Regular-Question-Answers/repeated-string-match.cpp
+++ Regular-Question-Answers/repeated-string-match.cpp
@@ -1,6 +1,6 @@
 // Author: Emre ÇELİK
 // Question Link: https://leetcode.com/problems/repeated-string-match/
-// Reviewers:
+// Reviewer: Denis Davidoglu
 
 // Time Complexity : O(n*m)
 // Space Complexity : O(1)
@@ -8,26 +8,24 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        int count; // We are declaring our answer attribute.
+        // We declare our answer attribute.
+        int count; 
         
         // Checking for special case and return answer.
-        if (b.empty()) {
-            return 1;
-        }
+        if (b.empty()) return 1;
         
         for (int i = 0; i < a.size(); i++) {
-            // Catching a string in b string with while loop.
+            // Catching string a in string b with while loop.
             while (i < a.size() && a[i] != b[0]) i++;
            
             int j = 0;
-            int k = i; // Assign our i value to k for protect i value.
+            int k = i; // This assignment saves i's value.
             count = 1; // Reassigning in every loop if there is separation.
-            while (a[k++] == b[j++]) { // Check a and b string are equal in loop.
-                // If we reached last element of b string then return answer.
-                if (j == b.size()) {
-                    return count;
-                } 
-                // If they are equal and a string is done make k is 0 and decrease count by 1.    
+            while (a[k++] == b[j++]) { // Check if strings a and b are equal in loop.
+                // If we reached last element of string b then return answer.
+                if (j == b.size()) return count;
+                
+                // If they are equal and string a is done, make k 0 and decrease count by 1.    
                 if (k == a.size()) {
                     k = 0; 
                     count++;
@@ -36,7 +34,7 @@ public:
             if (j > b.size()) break;
         }
         
-        // If we can't reach last element of b string with equality, return -1.
+        // If we can't reach last element of string b with equality, return -1.
         return -1;
     }
 };
