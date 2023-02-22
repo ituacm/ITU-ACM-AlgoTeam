4ee7aaef4afebcf3df564e73944a909d57315f9d
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-02-22 23:38:30 +0300

    Update jump-game-ii.cpp

--- Regular-Question-Answers/jump-game-ii.cpp
+++ Regular-Question-Answers/jump-game-ii.cpp
@@ -1,5 +1,6 @@
 // Author: Ceren Yaşar
 // Question Link: https://leetcode.com/problems/jump-game-ii/
+// Reviewer: Denis Davidoglu
 
 // Time Complexity: O(N*M) where N = nums.length and M = nums[i]
 // Space Complexity: O(N)
@@ -18,16 +19,15 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
             // Number in nums[i] denotes the maximum number of allowed jumps from that position.
-            // Meaning, the furthest point you will be able to reach from index i is (i + nums[i]).
-            // This means that you have a motion range of [1, nums[i]].
+            // The furthest point you will be able to reach from index i is (i + nums[i]).
+            // This means that you have a possible landing range of [1, nums[i]].
             int maxJumps = nums[i];
 
             // Iterating over every possible movement, i.e. from jumping 1 step ahead to jumping maxJumps steps ahead.
             for (int j = 1; j <= maxJumps; j++) {
                 // If the current position index exceeds the limits, break out of the loop to avoid segmentation fault.
-                if (i + j >= nums.size()) {
-                    break;
-                }
+                if (i + j >= nums.size()) break;
+                
                 // We now know that it is possible to reach the index (i+j) with jumps[i]+1 number of jumps. To ensure
                 // that we always store the minimum value so far, we should compare the number we have with the number that
                 // is already stored for the location i+j, which is jumps[i+j].
