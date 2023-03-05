a9c003d983145e075059cb97310560e2eba9f614
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-03-05 21:21:04 +0300

    Update search-insert-position.cpp

--- Regular-Question-Answers/search-insert-position.cpp
+++ Regular-Question-Answers/search-insert-position.cpp
@@ -1,6 +1,7 @@
-// Author: Novruz Amirov
 // ITU ACM AlgoTeam
-// Question link: https://leetcode.com/problems/search-insert-position/description/
+// Author: Novruz Amirov
+// Question link: https://leetcode.com/problems/search-insert-position/
+// Reviewer: Denis Davidoglu
 
 class Solution {
 public:
@@ -8,9 +9,9 @@ public:
         This is the binary search implementation question, 
         because of O(logN) time complexity given in the question.
 
-        Basically, in binary search we have 2 pointers left and right
+        Basically, in binary search we have 2 pointers left and right;
         left should always must be smaller or equal to right.
-        The array shoudl be in sorted as given in the question.
+        The array should be in sorted as given in the question.
     */
     int searchInsert(vector<int>& nums, int target) {
         // declaring left and right pointers
@@ -18,23 +19,21 @@ public:
         int right = nums.size() - 1;
 
         // left must be smaller or equal to right pointer
-        while(left <= right){
+        while (left <= right) {
             // checking mid results in logN time complexity
             int mid = (left + right) / 2; 
-            if(nums[mid] == target){
-                return mid; // target was found
-            }
-            else if(nums[mid] < target){
+            
+            if (nums[mid] == target) 
+                return mid; // target was found 
+            else if (nums[mid] < target)
                 left = mid+1; // target must be in right of mid
-            }
-            else{
+            else
                 right = mid-1; // target must be in left of mid
-            }
         }
 
         // if not found in given array
         return left; 
-
+        
         /*
             Overall Time Complexity: O(LogN)
             Space Complexity: O(1)
