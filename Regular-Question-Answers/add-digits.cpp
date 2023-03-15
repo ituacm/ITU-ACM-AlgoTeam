46bdf08b894578953b397061ec054b77c4a141c2
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-03-15 16:16:14 +0300

    Update and rename Add-Digits.cpp to add-digits.cpp

--- Regular-Question-Answers/add-digits.cpp
+++ Regular-Question-Answers/add-digits.cpp
@@ -1,17 +1,17 @@
 // Author: Emre Çelik
-// Reviewer: 
 // Question Link: https://leetcode.com/problems/add-digits/
-
+// Reviewer: Denis Davidoglu
 
 class Solution {
 public:
     int addDigits(int num) {
         // Initialize our num to our answer attribute
         int answer = num;
+        
         // While our answer is bigger than 9, do the our process.
-        while(answer>9){
+        while (answer > 9)
             answer = (answer%10) + (answer/10);
-        }
+            
         // Returning our answer 
         return answer;
     }
