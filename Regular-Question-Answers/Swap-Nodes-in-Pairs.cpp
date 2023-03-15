f35d2f9c1fa3bfa5651586b2dc780883fc8369f9
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-03-15 16:19:16 +0300

    Update Swap-Nodes-in-Pairs.cpp

--- Regular-Question-Answers/Swap-Nodes-in-Pairs.cpp
+++ Regular-Question-Answers/Swap-Nodes-in-Pairs.cpp
@@ -1,7 +1,6 @@
 // Author: Emre Çelik
-// Reviewer: 
 // Question Link: https://leetcode.com/problems/swap-nodes-in-pairs/
-
+// Reviewer: Denis Davidoglu
 
 /**
  * Definition for singly-linked list.
@@ -16,10 +15,10 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        // If rest of our linked list is empty or have one nosw, return it as a head. 
-        if(head == NULL or head->next == NULL){
+        // If rest of our linked list is empty or have one node, return it as a head. 
+        if (head == NULL || head->next == NULL) 
             return head;
-        }
+        
         // Initialize our head and next of our head.    
         ListNode* tempHead = head;
         ListNode* temp = tempHead->next;
@@ -28,7 +27,7 @@ public:
         tempHead->next = tempHead->next->next;
         temp->next = tempHead;
 
-        //Return next node as a head recursively.
+        // Return next node as a head recursively.
         tempHead->next = swapPairs(tempHead->next);
 
         return temp;
