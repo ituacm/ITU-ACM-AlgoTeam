4239640dd4ecb5576546ffab899f381de579de1c
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-03-27 19:33:10 +0300

    Update and rename single_number.cpp to single-number.cpp

--- Regular-Question-Answers/single-number.cpp
+++ Regular-Question-Answers/single-number.cpp
@@ -1,6 +1,6 @@
 // Author: Fatih Baskın
-// Reviewer:
 // Question Link: https://leetcode.com/problems/single-number/
+// Reviewer: Denis Davidoglu
 
 class Solution
 {
@@ -12,15 +12,14 @@ public:
         // numbers: 1 1 3 3 4 5 5
         // indexes: 0 1 2 3 4 5 6
         //                  ^
-        // As can be seen above, singe number will break the trend of side by side same numbers.
+        // As can be seen above, single number will break the trend of side by side same numbers.
         // Same numbers are located at 2*i'th and 2*i + 1'th indexes, i < n / 2.
         // Single number will break this trend, so we will look for this trend breaker.
 
         // Sorting operation takes nlog(n) time and checking for single number takes n/2 time, so complexity is O(nlog(n)).
 
         // Edge case, nums have only 1 element.
-        if (nums.size() == 1)
-            return nums[0];
+        if (nums.size() == 1) return nums[0];
 
         // Sort the nums vector.
         sort(nums.begin(), nums.end());
@@ -28,8 +27,7 @@ public:
         // We need to have the curr_idx as current index starting from 0 and last_idx as last index of the nums vector.
         int last_idx = nums.size() - 1;
         int curr_idx = 0;
-        while (curr_idx < last_idx)
-        {
+        while (curr_idx < last_idx) {
             // If the loop breaks, curr_idx would be the index of the trend breaker single number.
             if (nums[curr_idx] != nums[curr_idx + 1])
                 break;
