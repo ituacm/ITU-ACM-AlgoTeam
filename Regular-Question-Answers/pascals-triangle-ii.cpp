ee91a02ac8810d58ec8ec349abccbb27f1448b82
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-04-17 19:00:26 +0300

    Update pascals-triangle-ii.cpp

--- Regular-Question-Answers/pascals-triangle-ii.cpp
+++ Regular-Question-Answers/pascals-triangle-ii.cpp
@@ -1,30 +1,31 @@
-// Author : Ömer Faruk ERDEM
-// Question Link : "https://leetcode.com/problems/pascals-triangle-ii/"
+// Author: Ömer Faruk ERDEM
+// Question Link: https://leetcode.com/problems/pascals-triangle-ii/
+// Reviewer: Denis Davidoglu
 
 // Time Complexity : O(n^2)
 // Space Complexity : O(n^2)
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        // create a two dimensional array to store pascal triangle
+        // create a two dimensional array to store Pascal's triangle
         vector<vector<int>> pascalTriangle(rowIndex+1);
 
-        // Fill the pascal triangel according to its rule 
-        for(int i=0 ; i <= rowIndex ; i++){
+        // Fill the Pascal's triangle according to its rule 
+        for (int i = 0; i <= rowIndex; i++) {
             for(int j=0 ; j <= i ; j++){
                 // Set first elements value as one
-                if(j==0){
+                if (j == 0) {
                     pascalTriangle[i].push_back(1);
                     continue;
                 }   
                 // Set last elements value as one
-                if(j==i){
+                if (j == i) {
                     pascalTriangle[i].push_back(1);
                     continue;
                 }   
                 // Set middle elements value sum of the its ancestors at last row.
-                else{
-                    pascalTriangle[i].push_back( pascalTriangle[i-1][j-1] +pascalTriangle[i-1][j] );
+                else {
+                    pascalTriangle[i].push_back(pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]);
                 } 
             }
         }
