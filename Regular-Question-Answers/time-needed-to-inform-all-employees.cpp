727382c375bc86f17fbf0cf0c2f911fe13b1e209
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-04-23 14:33:01 +0300

    Update time-needed-to-inform-all-employees.cpp

--- Regular-Question-Answers/time-needed-to-inform-all-employees.cpp
+++ Regular-Question-Answers/time-needed-to-inform-all-employees.cpp
@@ -1,6 +1,6 @@
 // Author: Ali Kemal Coşkun
 // Question Link: https://leetcode.com/problems/time-needed-to-inform-all-employees/
-
+// Reviewer: Denis Davidoglu
 
 // Time complexity: O(N)
 // Space complexity: O(N)
@@ -15,30 +15,23 @@ public:
     // Regular DFS function.
     // The 'result' is sent as a parameter to the function.
     // You can also write the code so that 'res' is updated in the main function.
-    void dfs(vector<vector<int>>& adjList, vector<int>& informTime, int node, int time, int* res){
-        if(adjList[node].empty()){
+    void dfs(vector<vector<int>>& adjList, vector<int>& informTime, int node, int time, int* res) {
+        if (adjList[node].empty()) {
             *res = max(*res, time);
             return;
         }
-
-        for(int i=0;i<adjList[node].size();i++){
+        for (int i = 0; i < adjList[node].size(); i++)
             dfs(adjList, informTime, adjList[node][i], time+informTime[node], res);
-        }
     }
-
-
-
+    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<vector<int>> adjList(n, vector<int>()); 
 
-        
-        for(int i=0;i<n;i++){
-            //'head' has no manager.
-            if(i == headID) continue; 
+        for (int i = 0; i < n; i++) {
+            // 'head' has no manager.
+            if (i == headID) continue; 
             adjList[manager[i]].push_back(i);
         }
-
-
         int res = 0;
         dfs(adjList, informTime, headID, 0, &res);
 
