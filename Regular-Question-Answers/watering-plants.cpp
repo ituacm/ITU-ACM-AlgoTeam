9ece0533b19ca09371c04cf8878e559c2e5f1e55
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-05-19 14:14:06 +0300

    Update watering-plants.cpp

--- Regular-Question-Answers/watering-plants.cpp
+++ Regular-Question-Answers/watering-plants.cpp
@@ -1,10 +1,11 @@
 // Author: Serhat Sarı
 // Question Link: https://leetcode.com/problems/watering-plants/
+// Reviewer: Denis Davidoglu
 
 /*
 Here we hold a current_capacity variable to know the amount of water left inside bucket 
-and the variable steps. Start the loop from -1 because we start at the river. We check
-if our current capacity is bigger than the plant we will water next, if it is bigger
+and the steps variable. Start the loop from -1 because we start at the river. We check
+if our current capacity is bigger than the plant we will water next; if it is bigger,
 then we simply increase our step by one and decrease the water inside the bucket by the
 same amount of water needed for the plant.
 
@@ -14,25 +15,21 @@ from the index we are right now, (i+1) steps. Then we go back to the plant we we
 Then our current capacity is capacity ( full bucket ) - amount of water needed for the next plant.
 
 Time Complexity : O(n)
-
-
 */
 
 class Solution {
 public:
-
     int current_capacity;
     int steps = 0;
 
     int wateringPlants(vector<int>& plants, int capacity) {
         int n = plants.size();
         current_capacity = capacity;
-        for(int i = -1; i<n-1;i++){
-            if(current_capacity < plants[i+1]){
+        for (int i = -1; i < n-1; i++) {
+            if (current_capacity < plants[i+1]) {
                 steps += (i+1)*2 +1;
                 current_capacity = capacity - plants[i+1];
-            }
-            else{
+            } else {
                 steps++;
                 current_capacity -= plants[i+1];
             }
