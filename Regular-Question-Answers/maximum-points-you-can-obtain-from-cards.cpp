c87b9c0fe0619b4b853c975a14b825d407b98d33
Denis Davidoglu <39916800+dawidogg@users.noreply.github.com>
2023-05-20 13:42:54 +0300

    Update maximum-points-you-can-obtain-from-cards.cpp

--- Regular-Question-Answers/maximum-points-you-can-obtain-from-cards.cpp
+++ Regular-Question-Answers/maximum-points-you-can-obtain-from-cards.cpp
@@ -9,20 +9,20 @@ public:
     int maxScore(vector<int>& cardPoints, int k) {
         int result = 0;
 		
-		//Adding first k elements to result.
-        for(int i=0;i<k;i++){
+	// Adding first k elements to result.
+        for (int i = 0; i < k; i++) {
             result = result + cardPoints[i];
         } 
-        //Using current integer as a checker.
-        int current=result;
-        for(int i=k-1;i>=0;i--) {
-			//We remove the last visited element and add the non-visited
-            //element starting from the last
+        // Using current integer as a checker.
+        int current = result;
+        for (int i = k-1; i >= 0; i--) {
+	    // We remove the last visited element and add the non-visited
+            // element starting from the last
             current = current - cardPoints[i];
             current = current + cardPoints[cardPoints.size()-k+i];
 			
             //Checking which is maximum value and give it as a result.
-			result = max(result, current);
+	    result = max(result, current);
         }
         
         return result;
