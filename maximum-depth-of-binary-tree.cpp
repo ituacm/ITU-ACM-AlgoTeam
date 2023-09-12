885d7112caf2ad98b8f2f4faad2f5ee2f23143d4
İrem Taze <92296389+iremtaze@users.noreply.github.com>
2023-09-13 02:09:40 +0300

    maximum-depth-of-binary-tree.cpp

---
+++ maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,39 @@
+// Author: İrem Taze
+// Question Link: https://leetcode.com/problems/maximum-depth-of-binary-tree
+// Time Complexity : O(n) n= number of nodes, because we visit every node
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// finding maximum number function
+ int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+class Solution {
+public:
+    //Calculates the maximum depth of a binary tree.
+    int maxDepth(TreeNode* root) {
+    // if root is null pointer, terminate the program
+    if(root == nullptr) return 0;
+    
+    // apply recursive approach, substitute to subtrees
+    int max_left =  maxDepth(root->left);
+    int max_right =  maxDepth(root->right);
+    
+    //choose the maximum depth
+    int max_depth = max(max_left, max_right);
+
+    // add the current node
+    return max_depth + 1;
+
+
+    }
+};
