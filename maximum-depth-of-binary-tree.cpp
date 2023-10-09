2fe2a76091319c714308398ef641199eb5a31005
Berat Dalsuna <69195373+Enceladus-py@users.noreply.github.com>
2023-10-09 15:01:59 +0300

    fix indent

--- maximum-depth-of-binary-tree.cpp
+++ maximum-depth-of-binary-tree.cpp
@@ -12,28 +12,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 // finding maximum number function
- int max(int a, int b) {
+int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
 class Solution {
 public:
-    //Calculates the maximum depth of a binary tree.
+    // Calculates the maximum depth of a binary tree.
     int maxDepth(TreeNode* root) {
-    // if root is null pointer, terminate the program
-    if(root == nullptr) return 0;
-    
-    // apply recursive approach, substitute to subtrees
-    int max_left =  maxDepth(root->left);
-    int max_right =  maxDepth(root->right);
-    
-    //choose the maximum depth
-    int max_depth = max(max_left, max_right);
-
-    // add the current node
-    return max_depth + 1;
-
-
+      // if root is null pointer, terminate the program
+      if(root == nullptr) return 0;
+      
+      // apply recursive approach, substitute to subtrees
+      int max_left =  maxDepth(root->left);
+      int max_right =  maxDepth(root->right);
+      
+      // choose the maximum depth
+      int max_depth = max(max_left, max_right);
+  
+      // add the current node
+      return max_depth + 1;
     }
 };
