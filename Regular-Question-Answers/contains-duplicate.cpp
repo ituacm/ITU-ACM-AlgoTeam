0beb798caf5035cb6266bfae8b90f81619c00457
Tolga Kilinckaya <119072606+TolgaKilinckaya@users.noreply.github.com>
2023-10-13 13:04:01 +0300

    Update contains-duplicate.cpp

--- Regular-Question-Answers/contains-duplicate.cpp
+++ Regular-Question-Answers/contains-duplicate.cpp
@@ -1,24 +1,27 @@
-// Author: Mehmet Tolga kýlýnçkaya
+// Author: Mehmet Tolga kÃ½lÃ½nÃ§kaya
 // Reviewer:
 // Question Link: https://leetcode.com/problems/contains-duplicate/
 
-//time complexity -> O(n^2) (Brute Force)
+//time complexity -> O(n)
 class Solution {
 public:
+    // Function to check if a vector contains duplicate elements
     bool containsDuplicate(vector<int>& nums) {
-        // Get the size of the input vector.
-        int n = nums.size();
-
-        // Iterate through the vector to check for duplicates.
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                // Compare the elements at indices i and j.
-                if (nums[i] == nums[j])
-                    // Return true if a duplicate is found.
-                    return true;
-            }
+        // Create an unordered set to store the numbers we've seen
+        unordered_set<int> seen;
+        
+        // Iterate through each number in the vector
+        for (int num : nums) {
+            // Check if the current number is already in the 'seen' set
+            if (seen.count(num) > 0)
+                return true;  // If found, return true (a duplicate is present)
+            
+            // If not found, add the number to the 'seen' set
+            seen.insert(num);
         }
-        // If no duplicates are found after checking all pairs, return false.
+        
+        // If the loop completes without finding duplicates, return false
         return false;
     }
 };
+
