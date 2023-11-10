e177efbc6b42cc8a3fbe0dd7c52f2846f1591d34
yasarcereen <96792885+yasarcereen@users.noreply.github.com>
2023-11-10 14:05:05 +0300

    Fix spacing between operators

--- Regular-Question-Answers/longest-common-subsequence.cpp
+++ Regular-Question-Answers/longest-common-subsequence.cpp
@@ -11,16 +11,17 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int a = text1.length();
         int b = text2.length();
+     
         int dp[a + 1][b + 1];
         memset(dp, 0, sizeof(dp));
-        for (int i = a-1; i>=0; i--){
-            for(int j=b-1; j>=0; j--){
-                
-                if(text1[i]==text2[j]){
-                    dp[i][j]=dp[i+1][j+1]+1;    
+     
+        for (int i = a - 1; i >= 0; i--){
+            for(int j = b - 1; j >= 0; j--){
+                if(text1[i] == text2[j]){
+                    dp[i][j] = dp[i + 1][j + 1] + 1;    
                 }
                 else{
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
         }
