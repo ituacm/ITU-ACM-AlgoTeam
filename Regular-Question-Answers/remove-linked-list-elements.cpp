54e99b607805ab0d1157cd5925edbe1d9494badd
aliemre2023 <aliemrekaya239@gmail.com>
2023-11-22 00:09:25 +0300

    Ali Emre's Editorials 2 edited

--- Regular-Question-Answers/remove-linked-list-elements.cpp
+++ Regular-Question-Answers/remove-linked-list-elements.cpp
@@ -1,4 +1,5 @@
 // Author: Ali Emre Kaya
+// Reviewer: Ali Kemal Coşkun
 
 // Time Complexity = O(n)
 // Space Complexity = O(1)
@@ -14,34 +15,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
+Two consecutive node will hold and when the one in the front will find the 'val',
+    previous one will connect next of the front one,
+    this operation will continue until the end of the list and
+    all 'val' values will be deleted.
+*/
 class Solution {
-public:
+   public:
     ListNode* removeElements(ListNode* head, int val) {
         // prepare two node, these will follow each other
         ListNode* prev = nullptr;
         // use copy of head node to not lose head
         ListNode* curr = head;
-        
+
         // controlling entire list
-        while(curr != nullptr){
-            if(val == curr->val){
+        while (curr != nullptr) {
+            if (val == curr->val) {
                 // use copy of curr to not lose curr
                 ListNode* temp = curr->next;
-                if(prev != nullptr){
+                if (prev != nullptr) {
                     // omit the node which in the 'curr'
                     prev->next = temp;
-                } 
-                else{
-                    // if the value is in the head of the list, 
-                    //make the head the next of the head,
-                    //for to ignoring this value.
+                } else {
+                    // if the value is in the head of the list,
+                    // make the head the next of the head,
+                    // for to ignoring this value.
                     head = temp;
                 }
                 curr = temp;
-            } 
-            else{
+            } else {
                 // if curr's value is not 'val',
-                //both pointers should mark the next values
+                // both pointers should mark the next values
                 prev = curr;
                 curr = curr->next;
             }
