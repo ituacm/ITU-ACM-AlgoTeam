80abe4b3a849f038eb9d5501e823f6290ccd1b6d
Ashluu <92854043+Ashluu@users.noreply.github.com>
2024-01-06 11:50:35 +0300

    add comments

--- Regular-Question-Answers/best-time-to-buy-and-sell-stock-iii.cpp
+++ Regular-Question-Answers/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,6 +3,7 @@
 /*
 The dynamic programming approach efficiently calculates the maximum profit by considering the possibilities of buying and selling stocks for each transaction and each day, updating the maximum profit values based on the optimal decisions made at each step.
 */
+//dp[i][j] represents the maximum profit that can be obtained up to day j with at most i transactions.
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -17,6 +18,8 @@ public:
     vector<vector<int>> dp(k + 1, vector<int>(n, 0));
 
     for (int i = 1; i <= k; ++i) {
+          // Initialize the maximum difference to represent the maximum profit
+        // that can be obtained by buying at the current day
         int maxDiff = -prices[0];
 
         for (int j = 1; j < n; ++j) {
@@ -26,7 +29,7 @@ public:
             maxDiff = max(maxDiff, dp[i - 1][j] - prices[j]);
         }
     }
-    // The final result is stored in the last cell of the 2D vector
+    // The final result is stored in the last cell of the 2D vector. represents the maximum profit after completing all allowed transactions by the end of the given period.
     return dp[k][n - 1];
 }
   
