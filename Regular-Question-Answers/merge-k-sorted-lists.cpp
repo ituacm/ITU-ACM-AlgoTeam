e72737b45639133a6f259adc4677a17b9e491b34
Anıl Dervişoğlu <115112675+anildervis@users.noreply.github.com>
2024-03-16 22:43:45 +0300

    Update merge-k-sorted-lists.cpp

--- Regular-Question-Answers/merge-k-sorted-lists.cpp
+++ Regular-Question-Answers/merge-k-sorted-lists.cpp
@@ -3,8 +3,12 @@
 
 // If there were to be 2 distinct lists, we could have used two pointers method to sort it easily.
 // However, since there are k lists, we need to find the order of smallest elements in all of them in the least possible time complexity.
-// This can be done using a multiset or similar data structure in O(nlogn) complexity where n is the total number of elements in all lists.
+// This can be done using a multiset or similar data structure in O(n * logn) complexity where n is the total number of elements in all lists.
 // The idea is similar to merge sort, where we merge two lists in sorted order.
+
+// Comparing to one other method, we could have looked through first elements of each linked list and select the smallest one at each time,
+// This would result in O(n * k) where n is again the total number of elements in all lists and k is the number of all linked lists.
+// Again note that the solution complexities depend on the constraints but in this question the complexity of the following code is better.
 
 /**
  * Definition for singly-linked list.
