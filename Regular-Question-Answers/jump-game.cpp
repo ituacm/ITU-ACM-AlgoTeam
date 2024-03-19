ca515b2710a45ba5993f7a05aa6384125bd882a6
aliemre2023 <aliemrekaya239@gmail.com>
2024-03-19 15:54:20 +0300

    corrected thanks to Mr. Baskin's review

--- Regular-Question-Answers/jump-game.cpp
+++ Regular-Question-Answers/jump-game.cpp
@@ -1,9 +1,15 @@
 // Author: Ali Emre Kaya
-// Reviewer:
+// Reviewer: Fatih Baskın
 // Question Link: https://leetcode.com/problems/jump-game/
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+/*
+The problem gives us an array which has numbers, those numbers represent the maximum jump.
+We will try to reach the end of the list with using this jumps.
+Note that you can never go back since nums[i] >= 0 for every i.
+*/
 
 class Solution {
 public:
@@ -11,23 +17,19 @@ public:
         // if size of the array 1, directly return true
         if(nums.size() == 1) return true;
 
-        // current index you're standing at
-        int i = 0;
-        // each array element can be visit at most one time
-        //so time complexity is O(n)
-        while (i < nums.size()) {
-            // these variables will keep track of the maximum jump 
-            //reachable from the current index (i).
+        // each array element can be visit at most one time, so time complexity is O(n)
+        // curr_idx is current index you're standing at
+        for(int curr_idx = 0; curr_idx < nums.size();){
+            // these variables will keep track of the maximum jump, reachable from the current index.
             int best_jump = 0;
-            int best_jump_idx = i;
+            int best_jump_idx = curr_idx;
 
-            // for loop will iterate over potential jumps from i to i + nums[i]
-            for(int j = i + 1; j <= i + nums[i]; j++){
-                // if j jump is reachs furher than current jump,
-                //update variables
-                if(j + nums[j] > best_jump){
-                    best_jump = j + nums[j];
-                    best_jump_idx = j;
+            // for loop will iterate over potential jumps from (curr_index) to (curr_index + nums[curr_idx])
+            for(int reach_idx = curr_idx + 1; reach_idx <= curr_idx + nums[curr_idx]; reach_idx++){
+                // if reachable jump is reachs furher than current jump, update variables
+                if(reach_idx + nums[reach_idx] > best_jump){
+                    best_jump = reach_idx + nums[reach_idx];
+                    best_jump_idx = reach_idx;
                     // check if new jump can reach the end of the array
                     if(best_jump >= nums.size() - 1){
                         return true;
@@ -35,12 +37,12 @@ public:
                 }
             }
             // control if no jump can be made from the current position
-            if (best_jump_idx == i) {
+            if (best_jump_idx == curr_idx) {
                 return false;
             }
             
-            // update i to the best jump index found
-            i = best_jump_idx;
+            // update current index to the best jump index found
+            curr_idx = best_jump_idx;
         }
         // if you can't access the end of array return false
         return false;
